0c53c61aed1146f625e03359f69f828f80fb2851
Wiz0903 <162373446+Wiz0903@users.noreply.github.com>
2026-06-22 22:45:35 +0200

    Enhance navigation toggle with overlay and scroll control
    Added overlay functionality to navigation toggle.

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 const navigation = document.querySelector('.navigation');
 const toggleButton = document.querySelector('.menu-toggle');
+const overlay = document.querySelector('.overlay');
+const navLinks = document.querySelectorAll('.navigation a');
+const body = document.querySelector('body');
 
 toggleButton.addEventListener('click', () => {
     navigation.classList.toggle('active');
+    overlay.classList.toggle('active');
+    body.classList.toggle('no-scroll');
+});
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        navigation.classList.remove('active');
+        overlay.classList.remove('active');
+        body.classList.remove('no-scroll');
+    });
+});
+
+overlay.addEventListener('click', () => {
+    navigation.classList.remove('active');
+    overlay.classList.remove('active');
+    body.classList.remove('no-scroll');
 });
