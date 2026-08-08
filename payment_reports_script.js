d7657f5598b5cc661c64c440ef287cf7b988ec7b
Wiz0903 <162373446+Wiz0903@users.noreply.github.com>
2026-08-08 22:21:18 +0200

    Added payment reports files

---
+++ payment_reports_script.js
@@ -0,0 +1,60 @@
+const payments = localStorage.getItem("payments");
+const paymentsArray = JSON.parse(payments) || [];
+
+const monthFilter = document.getElementById('month-filter');
+const tableBody = document.querySelector("tbody");
+const noPayments = document.querySelector('.no-payments');
+const totalAmount = document.querySelector('.total-amount');
+
+const displayPaymentReport = (paymentsArray) => {
+    tableBody.innerHTML = "";
+    let totalCollected = 0;
+    let matchesFound = false;
+    let allMonths = false;
+
+    if (monthFilter.value === '') {
+        allMonths = true;
+    }
+
+    paymentsArray.forEach(payment => {
+        let paymentMatches = false
+
+        if (allMonths) {
+            paymentMatches = true
+        } else {
+            const paymentDate = new Date(payment.date);
+            const paymentMonth = paymentDate.getMonth();
+
+            if (paymentMonth === Number(monthFilter.value)) {
+                paymentMatches = true
+            }
+        }
+
+        if (paymentMatches) {
+            const row = document.createElement("tr");
+
+            row.innerHTML = `
+                <td>${payment.learnerName}</td>
+                <td>R${Number(payment.amount).toFixed(2)}</td>
+                <td>${payment.date}</td>
+                <td>${payment.paymentMethod}</td>
+            `;
+
+            totalCollected += Number(payment.amount);
+            tableBody.append(row);
+            matchesFound = true;
+        }
+    });
+
+    totalAmount.textContent = totalCollected.toFixed(2);
+
+    if (matchesFound) {
+        noPayments.classList.add('hide');
+    } else {
+        noPayments.classList.add('show');
+    }
+};
+
+monthFilter.addEventListener('change', () => {
+    displayPaymentReport(paymentsArray)
+});
