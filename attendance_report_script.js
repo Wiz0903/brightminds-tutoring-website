55b25fd45d32a932b3ce672b44b3e5ccdedb511a
Wiz0903 <162373446+Wiz0903@users.noreply.github.com>
2026-08-10 14:55:26 +0200

    Added attendance report files

---
+++ attendance_report_script.js
@@ -0,0 +1,70 @@
+const learners = localStorage.getItem('learners');
+const learnersArray = JSON.parse(learners);
+
+const attendance = localStorage.getItem("attendance");
+const attendanceArray = JSON.parse(attendance) || [];
+
+const tableBody = document.querySelector('tbody');
+const sessionsCount = document.querySelector('.sessions-count');
+const averagePercentage = document.querySelector('.average-percentage');
+const noAttendance = document.querySelector('.no-attendance');
+
+const sessionKeys = new Set();
+
+attendanceArray.forEach(attendance => {
+    const sessionKey = `${attendance.date} ${attendance.time}`
+
+    sessionKeys.add(sessionKey);
+});
+
+sessionsCount.textContent = sessionKeys.size;
+
+tableBody.innerHTML = "";
+let totalAttendancePercent = 0;
+let matchesFound = false;
+
+learnersArray.forEach(learner => {
+    let sessionsAttended = 0;
+
+    attendanceArray.forEach(attendance => {
+        if (`${learner.firstName} ${learner.lastName}` === `${attendance.learnerName}`) {
+            sessionsAttended++;
+        }
+    });
+
+    let attendancePercent = 0;
+
+    if (sessionKeys.size !== 0) {
+        attendancePercent = (sessionsAttended / sessionKeys.size) * 100;
+    }
+
+    const row = document.createElement("tr");
+
+    row.innerHTML = `
+        <td>${learner.firstName} ${learner.lastName}</td>
+        <td>${learner.grade}</td>
+        <td>${learner.school}</td>
+        <td>${sessionKeys.size}</td>
+        <td>${sessionsAttended}</td>
+        <td>${Number(attendancePercent).toFixed(2)}%</td>
+    `;
+
+    tableBody.append(row);
+    matchesFound = true;
+
+    totalAttendancePercent += attendancePercent;
+});
+
+let averageAttendance = 0;
+
+if (learnersArray.length !== 0) {
+    averageAttendance = totalAttendancePercent / learnersArray.length;
+}
+
+averagePercentage.textContent = `${Number(averageAttendance).toFixed(2)}`;
+
+if (matchesFound) {
+    noAttendance.classList.add('hide');
+} else {
+    noAttendance.classList.add('show');
+}
