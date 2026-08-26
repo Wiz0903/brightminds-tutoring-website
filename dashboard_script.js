641fe14ef246a7e140bf9d1b2df39aa1a6d72ed3
Wiz0903 <162373446+Wiz0903@users.noreply.github.com>
2026-08-26 12:41:44 +0200

    Refactor dashboard_script.js for improved readability

--- dashboard_script.js
+++ dashboard_script.js
@@ -124,24 +124,6 @@ logOUt.addEventListener('click', (event) => {
     window.location.href = "login.html";
 })
 
-const addRecentActivity = (activityType, activityMessage) => {
-    const today = new Date();
-    const isoString = today.toISOString();
-
-    const activity = {
-        type: activityType,
-        message: activityMessage,
-        date: isoString.slice(0, 10),
-        time: isoString.slice(11, 16)
-    }
-
-    const JSONstring = localStorage.getItem('activities')
-    const activityArray = JSON.parse(JSONstring) || [];
-    activityArray.push(activity);
-    const last20 = activityArray.slice(-20); 
-    localStorage.setItem('activities', JSON.stringify(last20));
-}
-
 const loadRecentActivities = () => {
     const JSONstring = localStorage.getItem('activities')
     const activityArray = JSON.parse(JSONstring) || [];
